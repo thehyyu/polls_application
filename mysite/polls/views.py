76d5d292c8f9e800dc3ac5f5cea3856a3f827db0
hyyu <hubert080116@gmail.com>
2019-07-21 20:07:32 +0800

    0721_polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,6 +23,12 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
 
+    def get_queryser(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
